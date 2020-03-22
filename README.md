85e3637ce867bee1bce6bf7aae24e5f6628a3c6b
Robert Krawczyk <robo22robo@yahoo.de>
2020-03-22 03:44:11 +0100

    Fetch in console: repositoryOwner(login: "nuwave")

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Challenge-React-app-with-GraphQL-API-v4-of-GitHub
+
+View:
+https://challenge-react-app-with-graph-ql-api-v4-of-git-hub.now.sh/
--- src/App.js
+++ src/App.js
@@ -5,6 +5,43 @@ import { createGlobalStyle } from "styled-components";
 import "styled-components/macro";
 import Login from "./Login";
 
+const accessToken = localStorage.getItem("token");
+
+fetch("https://api.github.com/graphql", {
+  method: "POST",
+  headers: {
+    Authorization: `bearer ${accessToken}`
+  },
+  body: JSON.stringify({
+    query: `
+    {
+      repositoryOwner(login: "nuwave"){
+      repository(name: "lighthouse") {
+         ref(qualifiedName: "master"){
+          associatedPullRequests(last: 10) {
+            edges {
+              node {
+                
+                bodyText
+                body
+                
+                mergeCommit {
+                  id
+                  message
+                }
+              }
+            }
+          }
+        }
+      }
+      }
+    }
+    `
+  })
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
+
 const Global = createGlobalStyle({
   body: {
     backgroundColor: "#fff",
@@ -23,7 +60,18 @@ function App() {
   return (
     <>
       <Global />
-      <Login />
+      {accessToken ? (
+        <button
+          onClick={() => {
+            localStorage.clear();
+            window.location.reload();
+          }}
+        >
+          Logout
+        </button>
+      ) : (
+        <Login />
+      )}
     </>
   );
 }
